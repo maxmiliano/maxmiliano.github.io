b80a68de91a3c68ae582d4a9342c087f6256df2e
Maxmiliano F Braga <maxmiliano@gmail.com>
2022-10-09 20:45:00 -0300

    Facatorial challeng post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
         "title": "Hello World!",
-        "excerpt":"Hello there, this my first blog post.   class HelloWorld    def initialize(name)       @name = name.capitalize    end    def say       puts \"Hello #{@name}!\"    end end  hello = HelloWorld.new(\"Max\") hello.say   ","categories": [],
-        "tags": ["ruby","helloWorld","code"],
+        "excerpt":"Hello there, this my first blog post.   class HelloWorld    def initialize(name)       @name = name.capitalize    end    def say       puts \"Hello #{@name}!\"    end end  hello = HelloWorld.new(\"Max\") hello.say  ","categories": [],
+        "tags": ["Ruby","hello_world","code"],
         "url": "/2022/09/30/hello-world.html",
         "teaser": "/assets/images/hello-world-ruby-code.png"
+      },{
+        "title": "How this blog was built",
+        "excerpt":"This blog is hosted on GitHub Pages and it was built with Jekyll following a tutorial from GitHub Skills. Jekyll is a static site generator that was made to be easy to use by developers. If you also want to quickly build a programming blog or a portfolio site to...","categories": [],
+        "tags": ["GitHub","meta"],
+        "url": "/2022/10/07/how-this-blog-was-built.html",
+        "teaser": "/assets/images/hello-world-ruby-code.png"
+      },{
+        "title": "Factorial Number code challenge",
+        "excerpt":"Finding the Factaorial Number is a common and simple challenge. Though it’s easy to solve I find it interesting because it allows us to solve it in different ways that explore a few programming concepts. In this video I show three different ways to solve this challenge. Check it out!...","categories": [],
+        "tags": ["Ruby","code challenge","YouTube","code"],
+        "url": "/2022/10/09/factorial-number-challenge.html",
+        "teaser": "/assets/images/2022-10-09-Factorial_Code_Challenge-thumb.png"
       }]
